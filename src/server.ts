fed68482ce1731ceecbf3a4397f908dd9ee85418
HemersonChaves <hemersonchaves20@hotmail.com>
2021-08-17 10:47:36 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+app.get('/', (request, response)=>{
+    return response.json({mensssage: "hello world"})
+})
+app.listen(3333, () => console.log("Server is running"))
