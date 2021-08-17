0b9e46d18d3fd7485b17edd33a539e3a2e5ce56d
HemersonChaves <hemersonchaves20@hotmail.com>
2021-08-17 13:44:19 -0300

    Adicionando arquivos de rotas

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,3 @@
 module.exports = {
     singleQuote: false,
-    trailingComma: 'all',
-    arrowParens: 'avoid',
   };  
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import express, { request, response } from "express";
 
 const app = express();
+app.use(express.json());
+
 
 app.get('/', (request, response)=>{
     return response.json({mensssage: "hello world"})
+})
+
+app.post("/courses", (request, response)=>{
+    
+    const {name} =request.body;
+
+    return response.json({name})
 })
 app.listen(3333, () => console.log("Server is running"))
