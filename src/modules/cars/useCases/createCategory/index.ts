d0ebed960eb18c507224506e566a9e79c9581fec
HemersonChaves <hemersonchaves20@hotmail.com>
2021-08-19 16:29:03 -0300

    Criando use case de categoria

---
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -0,0 +1,10 @@
+import { CategoriesRespository } from "../../repositories/CategoriesRepository"
+import { CreateCategoryController } from "./CreateCategoryController";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
+
+const categoriesRespository = new CategoriesRespository();
+const createCategoryUseCase = new CreateCategoryUseCase(categoriesRespository);
+
+const createCategoryController = new CreateCategoryController(createCategoryUseCase);
+
+export {createCategoryController}
