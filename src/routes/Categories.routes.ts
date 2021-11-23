a02ca9f83a0a4845f807be6d559607f52d8cec4d
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-23 11:15:51 -0300

    refactor(category): add console.log for viewer body params

--- src/routes/Categories.routes.ts
+++ src/routes/Categories.routes.ts
@@ -1,24 +1,28 @@
-import { request, response, Router } from "express";
-import multer  from 'multer';
-import { createCategoryController } from "../modules/cars/useCases/createCategory";
-import { listCategoriesController } from "../modules/cars/useCases/listCategories";
-import { importCategoryController } from "../modules/cars/useCases/importCategory";
+import { Router } from "express";
+import multer from 'multer';
+import createCategoryController from "../modules/cars/useCases/createCategory";
+import listCategoriesController from "../modules/cars/useCases/listCategories";
+import importCategoryController from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
 
 const upload = multer({ dest: './tmp' })
 
 categoriesRoutes.post("/", (request, response) => {
-   return createCategoryController.handle(request, response);
+    console.log(request.body.name);
+    console.log(request.body.description);
+    // request.body.description = "desc";
+    // request.body.name ="hemerson";
+    return createCategoryController().handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-    return listCategoriesController.handle(request, response);
+    return listCategoriesController().handle(request, response);
 });
 
-categoriesRoutes.post("/import", upload.single("file"), (request, response)=>{
-    return importCategoryController.handle(request, response);
-    
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    return importCategoryController().handle(request, response);
+
 })
 
 export { categoriesRoutes }
