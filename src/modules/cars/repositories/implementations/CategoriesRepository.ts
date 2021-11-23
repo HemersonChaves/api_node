fb8d0d2537cae331276c4af7411a022ddd1b84a0
Hemerson Chaves <hemersonchaves20@hotmail.com>
2021-11-23 11:00:52 -0300

    refactor(category): add typeorm for consume database

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,32 +1,27 @@
-import { Category}from "../../entities/Category"
+import { getRepository, Repository } from "typeorm";
+import { Category } from "../../entities/Category"
 import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
 
-
-class CategoriesRespository implements ICategoriesRepository{
-    private categories: Category[];
+class CategoriesRespository implements ICategoriesRepository {
     private static INSTANCE: CategoriesRespository;
+    private repository: Repository<Category>;
+    constructor() {
+        this.repository = getRepository(Category)
+    }
+    async create({ name, description }: ICreateCategoryDTO): Promise<void> {
 
-    private constructor(){
-        this.categories = [];
+        const category = this.repository.create({ name, description });
+        await this.repository.save(category);
     }
-    public static getInstance():CategoriesRespository{
-        if(!CategoriesRespository.INSTANCE){
-            CategoriesRespository.INSTANCE = new CategoriesRespository();
-        }
-        return CategoriesRespository.INSTANCE;
+    async list(): Promise<Category[]> {
+        const categories = await this.repository.find();
+        return categories;
     }
-    create({name, description}: ICreateCategoryDTO):void{
-        const category = new Category();
-        Object.assign(category, { name, description })
-        this.categories.push(category)
-    }
-    list(): Category[]{
-        return this.categories;
-    }
-    findByName(name:string): Category{
-        const category = this.categories.find(category => category.name === name);
+    async findByName(name: string): Promise<Category> {
+        //select * from categories where name = "name" limit 1
+        const category = <Category> await this.repository.findOne({name});//<Category>this.categories.find(category => category.name === name);        
         return category;
     }
 }
 
-export{CategoriesRespository}
+export { CategoriesRespository }
